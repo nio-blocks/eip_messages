d20fd45a1de0e3916807e1728d065d05576d6f5d
Tom Young <tyoung@n.io>
2018-04-03 17:32:31 -0600

    create get_attr block class

--- get_attribute_block.py
+++ get_attribute_block.py
@@ -1,12 +1,33 @@
+from pycomm.cip.cip_generic import Driver
 from nio import Block
-from nio.properties import VersionProperty
+from nio.properties import Property, IntProperty, StringProperty, \
+                           VersionProperty
 
 
 class GetAttribute(Block):
 
     version = VersionProperty('0.1.0')
+    host = StringProperty(title='Hostname', default='localhost')
+    class_id = IntProperty(title='Class ID', default=1)
+    instance_num = IntProperty(title='Instance', default=1)
+    attribute_num = Property(title='Attribute', default=None, allow_none=True)
+
+    def __init__(self):
+        super().__init__()
+        self.conn = Driver()
+
+    def configure(self, context):
+        super().configure(context)
+        self.conn.open(self.host())
 
     def process_signals(self, signals):
+        path = [self.class_id(), self.instance_num()]
+        if self.attribute_num() != None:
+            path.append(int(self.attribute_num()))
         for signal in signals:
-            pass
+            self.conn.get_attribute_single(*path)
         self.notify_signals(signals)
+
+    def stop(self):
+        super().stop()
+        self.conn.close()
--- tests/test_get_attribute_block.py
+++ tests/test_get_attribute_block.py
@@ -1,19 +1,41 @@
-from nio.block.terminals import DEFAULT_TERMINAL
-from nio.signal.base import Signal
+from unittest.mock import patch
+from nio import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
 from ..get_attribute_block import GetAttribute
 
 
 class TestGetAttribute(NIOBlockTestCase):
 
-    def test_process_signals(self):
+    @patch(GetAttribute.__module__ + '.Driver')
+    def test_defaults(self, mock_driver):
         """Signals pass through block unmodified."""
+        drvr = mock_driver.return_value
         blk = GetAttribute()
         self.configure_block(blk, {})
         blk.start()
-        blk.process_signals([Signal({"hello": "nio"})])
+        drvr.open.assert_called_once_with('localhost')
+        blk.process_signals([Signal()])
+        drvr.get_attribute_single.assert_called_once_with(1, 1)
         blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "nio"})
+        drvr.close.assert_called_once_with()
+
+    @patch(GetAttribute.__module__ + '.Driver')
+    def test_configured_block(self, mock_driver):
+        """Signals pass through block unmodified."""
+        drvr = mock_driver.return_value
+        config = {
+            'host': 'dummyhost',
+            'class_id': 8,
+            'instance_num': 6,
+            'attribute_num': 7}
+        blk = GetAttribute()
+        self.configure_block(blk, config)
+        blk.start()
+        drvr.open.assert_called_once_with(config['host'])
+        blk.process_signals([Signal()])
+        drvr.get_attribute_single.assert_called_once_with(
+            config['class_id'],
+            config['instance_num'],
+            config['attribute_num'])
+        blk.stop()
+        drvr.close.assert_called_once_with()
