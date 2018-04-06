922b18b74cbe0f74ace2e126ed6d6660650ba10b
Tom Young <tyoung@n.io>
2018-04-06 14:24:04 -0600

    refactor unit tests

--- tests/test_get_attribute_block.py
+++ tests/test_get_attribute_block.py
@@ -8,20 +8,51 @@ from ..get_attribute_block import GetAttribute
 class TestGetAttribute(NIOBlockTestCase):
 
     @patch(GetAttribute.__module__ + '.Driver')
-    def test_defaults(self, mock_driver):
-        """Default block behavior"""
+    def test_block_expressions(self, mock_driver):
+        """Get an attribute from the specified path and host"""
         drvr = mock_driver.return_value
-        drvr.get_attribute_single.return_value = 42
+        drvr.get_attribute_single.return_value = 5309
+        config = {
+            'host': 'dummyhost',
+            'class_id': '{{ $class_id }}',
+            'instance_num': '{{ $instance_num }}',
+            'attribute_num': '{{ $attribute_num }}'}
+        blk = GetAttribute()
+        self.configure_block(blk, config)
+        blk.start()
+        drvr.open.assert_called_once_with('dummyhost')
+        incoming_signal = Signal({
+            'class_id': 8, 'instance_num': 6, 'attribute_num': 7})
+        blk.process_signals([incoming_signal])
+        drvr.get_attribute_single.assert_called_once_with(8, 6, 7)
+        blk.stop()
+        drvr.close.assert_called_once_with()
+        self.assert_last_signal_notified(Signal(
+            {'host': 'dummyhost', 'path': [8, 6, 7], 'value': 5309}))
+
+    @patch(GetAttribute.__module__ + '.Driver')
+    def test_signal_lists(self, mock_driver):
+        """Outgoing signal lists have the same length as incoming"""
         blk = GetAttribute()
         self.configure_block(blk, {})
         blk.start()
-        drvr.open.assert_called_once_with('localhost')
-        blk.process_signals([Signal()])
-        drvr.get_attribute_single.assert_called_once_with(1, 1)
+        blk.process_signals([Signal()] * 3)
         blk.stop()
-        drvr.close.assert_called_once_with()
-        self.assert_last_signal_notified(
-            Signal({'host': 'localhost', 'path': [1, 1], 'value': 42}))
+        self.assertEqual(len(self.notified_signals[DEFAULT_TERMINAL]), 1)
+        self.assertEqual(len(self.notified_signals[DEFAULT_TERMINAL][0]), 3)
+
+    @patch(GetAttribute.__module__ + '.Driver')
+    def test_signal_enrichment(self, mock_driver):
+        """Incoming signals are enriched new data"""
+        drvr = mock_driver.return_value
+        drvr.get_attribute_single.return_value = 42
+        blk = GetAttribute()
+        self.configure_block(blk, {'enrich': {'exclude_existing': False}})
+        blk.start()
+        blk.process_signals([Signal({'foo': 'bar'})])
+        blk.stop()
+        self.assert_last_signal_notified(Signal(
+            {'foo': 'bar', 'host': 'localhost', 'path': [1, 1], 'value': 42}))
 
     @patch(GetAttribute.__module__ + '.Driver')
     def test_failure_to_get(self, mock_driver):
@@ -36,72 +67,3 @@ class TestGetAttribute(NIOBlockTestCase):
         self.assertEqual(drvr.get_attribute_single.call_count, 2)
         blk.stop()
         self.assert_num_signals_notified(1)
-
-    @patch(GetAttribute.__module__ + '.Driver')
-    def test_configured_block(self, mock_driver):
-        """Get an attribute from the specified path and host"""
-        drvr = mock_driver.return_value
-        drvr.get_attribute_single.return_value = 5309
-        config = {
-            'host': 'dummyhost',
-            'class_id': 8,
-            'instance_num': 6,
-            'attribute_num': 7}
-        blk = GetAttribute()
-        self.configure_block(blk, config)
-        blk.start()
-        drvr.open.assert_called_once_with(config['host'])
-        blk.process_signals([Signal()])
-        drvr.get_attribute_single.assert_called_once_with(
-            config['class_id'],
-            config['instance_num'],
-            config['attribute_num'])
-        blk.stop()
-        drvr.close.assert_called_once_with()
-        self.assert_last_signal_notified(
-            Signal({'host': 'dummyhost', 'path': [8, 6, 7], 'value': 5309}))
-
-    @patch(GetAttribute.__module__ + '.Driver')
-    def test_block_expressions(self, mock_driver):
-        """Get an attribute from the specified path and host"""
-        drvr = mock_driver.return_value
-        drvr.get_attribute_single.return_value = 5309
-        config = {
-            'class_id': '{{ $class_id }}',
-            'instance_num': '{{ $instance_num }}',
-            'attribute_num': '{{ $attribute_num }}'}
-        blk = GetAttribute()
-        self.configure_block(blk, config)
-        blk.start()
-        incoming_signal = Signal({
-            'class_id': 8, 'instance_num': 6, 'attribute_num': 7})
-        blk.process_signals([incoming_signal])
-        blk.stop()
-        drvr.close.assert_called_once_with()
-        self.assert_last_signal_notified(
-            Signal({'host': 'localhost', 'path': [8, 6, 7], 'value': 5309}))
-
-    @patch(GetAttribute.__module__ + '.Driver')
-    def test_signal_lists(self, mock_driver):
-        """Outgoing signal lists have the same length as incoming"""
-        blk = GetAttribute()
-        self.configure_block(blk, {})
-        blk.start()
-        blk.process_signals([Signal()] * 3)
-        blk.stop()
-        self.assertEqual(len(self.notified_signals[DEFAULT_TERMINAL]), 1)
-        for signal_list in self.notified_signals[DEFAULT_TERMINAL]:
-            self.assertEqual(len(signal_list), 3)
-
-    @patch(GetAttribute.__module__ + '.Driver')
-    def test_signal_enrichment(self, mock_driver):
-        """Incoming signals are enriched new data"""
-        drvr = mock_driver.return_value
-        drvr.get_attribute_single.return_value = 42
-        blk = GetAttribute()
-        self.configure_block(blk, {'enrich': {'exclude_existing': False}})
-        blk.start()
-        blk.process_signals([Signal({'foo': 'bar'})])
-        blk.stop()
-        self.assert_last_signal_notified(Signal(
-            {'foo': 'bar', 'host': 'localhost', 'path': [1, 1], 'value': 42}))
--- tests/test_set_attribute_block.py
+++ tests/test_set_attribute_block.py
@@ -8,40 +8,11 @@ from ..set_attribute_block import SetAttribute
 class TestSetAttribute(NIOBlockTestCase):
 
     @patch(SetAttribute.__module__ + '.Driver')
-    def test_default_block(self, mock_driver):
-        """Set an attribute using the default values"""
-        drvr = mock_driver.return_value
-        drvr.set_attribute_single.return_value = True
-        blk = SetAttribute()
-        self.configure_block(blk, {})
-        blk.start()
-        drvr.open.assert_called_once_with('localhost')
-        blk.process_signals([Signal()])
-        drvr.set_attribute_single.assert_called_once_with(b'\x00\x00', 1, 1)
-        blk.stop()
-        drvr.close.assert_called_once_with()
-        self.assert_last_signal_notified(Signal(
-            {'host': 'localhost', 'path': [1, 1], 'value': b'\x00\x00'}))
-
-    @patch(SetAttribute.__module__ + '.Driver')
-    def test_failure_to_set(self, mock_driver):
-        """One of two requests fail"""
-        drvr = mock_driver.return_value
-        drvr.set_attribute_single.side_effect = [False, True]
-        drvr.get_status.return_value = (1, 'bad things')
-        blk = SetAttribute()
-        self.configure_block(blk, {})
-        blk.start()
-        blk.process_signals([Signal()] * 2)
-        self.assertEqual(drvr.set_attribute_single.call_count, 2)
-        blk.stop()
-        self.assert_num_signals_notified(1)
-
-    @patch(SetAttribute.__module__ + '.Driver')
     def test_block_expressions(self, mock_driver):
         """Set an attribute value from the specified path, host, and value"""
         drvr = mock_driver.return_value
         config = {
+            'host': 'dummyhost',
             'class_id': '{{ $class_id }}',
             'instance_num': '{{ $instance_num }}',
             'attribute_num': '{{ $attribute_num }}',
@@ -49,16 +20,19 @@ class TestSetAttribute(NIOBlockTestCase):
         blk = SetAttribute()
         self.configure_block(blk, config)
         blk.start()
+        drvr.open.assert_called_once_with('dummyhost')
         incoming_signal = Signal({
             'class_id': 8,
             'instance_num': 6,
             'attribute_num': 7,
             'value': bytes([5, 3, 0, 9])})
         blk.process_signals([incoming_signal])
+        drvr.set_attribute_single.assert_called_once_with(
+            bytes([5, 3, 0, 9]), 8, 6, 7)
         blk.stop()
         drvr.close.assert_called_once_with()
         self.assert_last_signal_notified(Signal({
-            'host': 'localhost',
+            'host': 'dummyhost',
             'path': [8, 6, 7],
             'value': bytes([5, 3, 0, 9])}))
 
@@ -88,3 +62,17 @@ class TestSetAttribute(NIOBlockTestCase):
             'host': 'localhost',
             'path': [1, 1],
             'value': b'\x00\x00'}))
+
+    @patch(SetAttribute.__module__ + '.Driver')
+    def test_failure_to_set(self, mock_driver):
+        """One of two requests fail"""
+        drvr = mock_driver.return_value
+        drvr.set_attribute_single.side_effect = [False, True]
+        drvr.get_status.return_value = (1, 'bad things')
+        blk = SetAttribute()
+        self.configure_block(blk, {})
+        blk.start()
+        blk.process_signals([Signal()] * 2)
+        self.assertEqual(drvr.set_attribute_single.call_count, 2)
+        blk.stop()
+        self.assert_num_signals_notified(1)
