9c93b0cf2035bd8845998b6cafa3eddbf22c78d8
neutralioJenkins <jenkins@n.io>
2018-04-10 09:30:56 -0600

    Bump version to v0.1.1

--- eip_set_attribute_block.py
+++ eip_set_attribute_block.py
@@ -7,7 +7,7 @@ from nio.properties import Property, IntProperty, StringProperty, \
 
 class EIPSetAttribute(EnrichSignals, Block):
 
-    version = VersionProperty('0.1.0')
+    version = VersionProperty("0.1.1")
     host = StringProperty(title='Hostname', default='localhost')
     class_id = IntProperty(title='Class ID', default=1)
     instance_num = IntProperty(title='Instance', default=1)
